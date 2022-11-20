e210a47989cd6c217f749ad783fd63d17021181f
Dang The Vinh <dangthevinh12.com>
2022-11-20 13:37:50 +0700

    Change Logo

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import NProgress from "nprogress";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass: "active",
+  linkExactActiveClass: "exact-active",
   routes: [
     {
       path: "/",
@@ -13,6 +15,38 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/blog",
+      name: "blog",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/career",
+      name: "career",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/experience",
+      name: "experience",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/work",
+      name: "work",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
